d0f4e9014752aed9c82885a6abb20298f4e4b8c2
Lenovo <@>
2023-02-01 03:00:47 +0300

    dz вывод данных о городе

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
+    appid = 'dc7a59a4550ae730509e0659efcf4aa7'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city = 'London'
+    my_city = 'Gomel'
+    res = requests.get(url.format(my_city))
+    print(res.text)
     return render(request, 'weather/index.html')
 
 
